241caf61d2c088014b629ff9f2eb634c6a12b1d9
Sandra Emry <s.emry07@gmail.com>
2016-09-29 20:38:25 -0700

    r markdown of graphics homework

--- scripts/hw01_script.R
+++ scripts/hw01_script.R
@@ -5,50 +5,40 @@ seastars <- read.csv(file = "./data/hw01_data.csv")
 feeding <- read.csv(file = "./data/hw01_data_2.csv")
 suppressMessages(library(tidyverse))
 
+str(seastars)
 str(feeding)
 as.factor(feeding$Prey_size)
 levels(feeding$prey_size)
+
+n_unique(seastars$mussel_size_mm)
+?unique
 
 feeding <- feeding %>% 
   rename(prey_size = Prey_size, 
          prof = Profitability_g_per_min,
          feed_time = feeding_time_1_hr_d, 
-         hand_time = Handling_time_min) %>% 
-  as.factor(., prey_size)
+         hand_time = Handling_time_min) 
 
-feed_mod <- feeding %>% 
-  select(prey_size, prof) %>% View
+View(feeding)
+View(seastars)
+
+boxplot( feeding$hand_time ~ feeding$prey_size)
 
 plot3 <- ggplot(feed_mod, aes(x = as.factor(prey_size), y = prof)) + 
-  geom_point() + 
+  geom_boxplot() + 
   ylab("profitability (g tissue/hr)") + 
   xlab("mussel size class") + 
   scale_x_discrete(labels=c("10" = "10 mm", "15" = "15 mm",
                               "20" = "20 mm", "25" = "25 mm")) +
   theme_bw()
 
-ggplot(feed_mod, aes(x = as.factor(prey_size), y = prof)) + 
-  geom_point() + 
-  ylab("profitability (g tissue/hr)") + 
-  xlab("mussel size class") + 
-  scale_x_discrete(labels=c("10" = "10 mm", "15" = "15 mm",
-                            "20" = "20 mm", "25" = "25 mm")) +
-  theme_bw()
-
-
-
-
-
-plot2 <- (feeding, aes(x = as.factor(prey_size), y = hand_time)) + 
-  geom_point() + 
+plot2 <- (feeding, aes(x = as.factor(prey_size), y = as.numeric(hand_time))) + 
+  geom_boxplot() + 
   ylab("handling time (min/mussel)") +
   xlab("mussel size class") + 
   scale_x_discrete(labels=c("10" = "10 mm", "15" = "15 mm",
                             "20" = "20 mm", "25" = "25 mm")) +
   theme_bw()
-
-str(feeding)
-feeding$hand_time
 
 
 

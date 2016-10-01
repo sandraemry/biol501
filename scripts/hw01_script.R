b73a8457e0a4df994fe92c21773b5fb2f7f385d8
Sandra Emry <s.emry07@gmail.com>
2016-09-30 18:02:51 -0700

    finished hw01 with a word doc

--- scripts/hw01_script.R
+++ scripts/hw01_script.R
@@ -1,45 +1,45 @@
 
-# load in data ------------------------------------------------------------
+# load in data and packages ------------------------------------------------------------
 
-seastars <- read.csv(file = "./data/hw01_data.csv")
 feeding <- read.csv(file = "./data/hw01_data_2.csv")
 suppressMessages(library(tidyverse))
 
-str(seastars)
+# Look at the data... ------------------------------------------------------------
+
+# load packages
+suppressMessages(library(tidyverse))
+
+# examine data
 str(feeding)
-as.factor(feeding$Prey_size)
-levels(feeding$prey_size)
 
-n_unique(seastars$mussel_size_mm)
-?unique
-
+# rename variables
 feeding <- feeding %>% 
   rename(prey_size = Prey_size, 
          prof = Profitability_g_per_min,
          feed_time = feeding_time_1_hr_d, 
-         hand_time = Handling_time_min) 
+         hand_time = Handling_time_min,
+         seastar = Seastar) 
 
-View(feeding)
-View(seastars)
 
-boxplot( feeding$hand_time ~ feeding$prey_size)
+# New Plots ---------------------------------------------------------------
 
-plot3 <- ggplot(feed_mod, aes(x = as.factor(prey_size), y = prof)) + 
-  geom_boxplot() + 
+# Box plot of handling time vs mussel size 
+ggplot(feeding, aes(x = as.factor(prey_size), y = hand_time)) + 
+  geom_boxplot(outlier.colour = "green") +
+  geom_jitter(position = position_jitter(width = 0, height = 0), alpha = 1/2) + 
+  ylab("handling time (min/mussel)") +
+  xlab("mussel size class") +
+  scale_x_discrete(labels = c("10" = "10 mm", "15" = "15 mm",
+                              "20" = "20 mm", "25" = "25 mm")) +
+  theme_bw() +
+  expand_limits(y = c(0, 500))
+
+# Box plot of profitability vs mussel size
+ggplot(feeding, aes(x = as.factor(prey_size), y = prof)) + 
+  geom_boxplot(outlier.colour = "green") + 
+  geom_jitter(position = position_jitter(width = 0, height = 0), alpha = 1/2) + 
   ylab("profitability (g tissue/hr)") + 
   xlab("mussel size class") + 
-  scale_x_discrete(labels=c("10" = "10 mm", "15" = "15 mm",
+  scale_x_discrete(labels = c("10" = "10 mm", "15" = "15 mm",
                               "20" = "20 mm", "25" = "25 mm")) +
   theme_bw()
-
-plot2 <- (feeding, aes(x = as.factor(prey_size), y = as.numeric(hand_time))) + 
-  geom_boxplot() + 
-  ylab("handling time (min/mussel)") +
-  xlab("mussel size class") + 
-  scale_x_discrete(labels=c("10" = "10 mm", "15" = "15 mm",
-                            "20" = "20 mm", "25" = "25 mm")) +
-  theme_bw()
-
-
-
-
